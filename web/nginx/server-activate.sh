2eb72f871a9e26e45b7d6e1af27d89fc784c176e
Chan <69744429+000chan@users.noreply.github.com>
2022-11-02 17:59:34 +0900

    Update server-activate.sh

--- web/nginx/server-activate.sh
+++ web/nginx/server-activate.sh
@@ -1,6 +1,6 @@
-sudo python3 makemigrations
-sudo python3 migrate
+# sudo python3 makemigrations
+# sudo python3 migrate
 
-uwsgi --http 0.0.0.0:8000 --module config.uwsgi
+uwsgi --http 0.0.0.0:8000 --module config.wsgi &
 
 # cd /home/ychan/CapstoneProject/web/was/
