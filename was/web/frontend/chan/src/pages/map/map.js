1584ce297a63292dc3524374e6e7ccf1e6c3c3f8
김영찬-PC\김영찬-PC <ychan@tricycle.co.kr>
2022-11-08 18:03:24 +0900

    map.js 코드 리팩토링

--- was/web/frontend/chan/src/pages/map/map.js
+++ was/web/frontend/chan/src/pages/map/map.js
@@ -1,21 +1,40 @@
 /*global kakao*/
 import React, { useEffect } from 'react'
 
-const Map=()=>{
-    // 컴포넌트 렌더링 시, 특정 작업을 실행시키는 Hook
-    useEffect(()=>{
-        var container = document.getElementById('map');
-        var options = {
-            center: new kakao.maps.LatLng(37.552308561482235, 126.94784341756501),
-            level: 3
-        };
-        var map = new kakao.maps.Map(container, options);
-    }, [])
+function makeMap(latitude, longitude) {
+    let container = document.getElementById('map');
+    let options = {
+        center: new kakao.maps.LatLng(latitude, longitude),
+        level: 3
+    };
+    let map = new kakao.maps.Map(container, options);
+    return map;
+}
 
+const Map=()=>{
+
+    // 위도
+    let latitude = 37.552308561482235;
+    // 경도
+    let longitude = 126.94784341756501;
+
+    useEffect(() => {
+        makeMap(latitude, longitude);
+    }, []);
+
+    // 컴포넌트 렌더링 시, 특정 작업을 실행시키는 Hook
+    // useEffect(()=>{
+    //     let container = document.getElementById('map');
+    //     let options = {
+    //         center: new kakao.maps.LatLng(37.552308561482235, 126.94784341756501),
+    //         level: 3
+    //     };
+    //     let map = new kakao.maps.Map(container, options);
+    // }, []);
 
     return (
         <div>
-            <div id="map" style={{width:"500px", height:"400px"}}></div>
+            <div id="map" style={{width:"500px", height:"400px"}}/>
         </div>
     )
 }
