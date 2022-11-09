4ad7fefc296451826bb39588d80787bb26560a0a
김영찬-PC\김영찬-PC <ychan@tricycle.co.kr>
2022-11-09 18:12:34 +0900

    react 페이지 작업 진행사항 (loginPage, aboutPage, mapPage, myPage)

--- was/web/frontend/chan/src/pages/about/about.js
+++ was/web/frontend/chan/src/pages/about/about.js
@@ -1,17 +1,28 @@
-import React, {useState} from 'react'
+import React from 'react'
+// import antd
+import {Menu, Carousel} from 'antd';
 import {
-    BarsOutlined,
-    CompassOutlined,
-    HomeOutlined,
+    AppstoreOutlined,
     SettingOutlined,
+    CompassOutlined,
     SolutionOutlined,
+    HomeOutlined,
+    BarsOutlined,
     ToolOutlined,
     TeamOutlined
 } from '@ant-design/icons';
-import {Menu, Switch} from 'antd';
 import 'antd/dist/antd.css';
 
-const Mypage = () => {
+// carousel 설정
+const contentStyle = {
+    height: '160px',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
+};
+
+const About = () => {
     return (
         <>
             {/*상단 메뉴*/}
@@ -37,9 +48,24 @@ const Mypage = () => {
                     Mypage
                 </Menu.Item>
             </Menu>
-            <h1>MyPage</h1>
+            <h1>AboutPage</h1>
+            {/*  본문  */}
+            <Carousel autoplay>
+                <div>
+                    <h3 style={contentStyle}>안영훈</h3>
+                </div>
+                <div>
+                    <h3 style={contentStyle}>김영찬</h3>
+                </div>
+                <div>
+                    <h3 style={contentStyle}>백수연</h3>
+                </div>
+                <div>
+                    <h3 style={contentStyle}>신재현</h3>
+                </div>
+            </Carousel>
         </>
-    );
+    )
 }
 
-export default Mypage
+export default About
--- was/web/frontend/chan/src/pages/map/map.js
+++ was/web/frontend/chan/src/pages/map/map.js
@@ -1,5 +1,47 @@
 /*global kakao*/
-import React, { useEffect } from 'react'
+import React, {useEffect, useState} from 'react'
+import {
+    AppstoreOutlined, BarsOutlined,
+    CompassOutlined,
+    HomeOutlined,
+    MailOutlined,
+    SettingOutlined,
+    SolutionOutlined,
+    ToolOutlined,
+    TeamOutlined
+} from '@ant-design/icons';
+import {Menu, Switch} from 'antd';
+import 'antd/dist/antd.css';
+
+function getItem(label, key, icon, children, type) {
+    return {
+        key,
+        icon,
+        children,
+        label,
+        type,
+    };
+}
+
+const items = [
+    getItem('메뉴 1', 'sub1', <MailOutlined/>, [
+        getItem('소메뉴 1', '1'),
+        getItem('소메뉴 2', '2'),
+        getItem('소메뉴 3', '3'),
+        getItem('소메뉴 4', '4'),
+    ]),
+    getItem('메뉴 2', 'sub2', <AppstoreOutlined/>, [
+        getItem('소메뉴 5', '5'),
+        getItem('소메뉴 6', '6'),
+        getItem('서브메뉴', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
+    ]),
+    getItem('메뉴 3', 'sub4', <SettingOutlined/>, [
+        getItem('소메뉴 9', '9'),
+        getItem('소메뉴 10', '10'),
+        getItem('소메뉴 11', '11'),
+        getItem('소메뉴 12', '12'),
+    ]),
+];
 
 function makeMap(latitude, longitude) {
     let container = document.getElementById('map');
@@ -11,7 +53,7 @@ function makeMap(latitude, longitude) {
     return map;
 }
 
-const Map=()=>{
+const Map = () => {
 
     // 위도
     let latitude = 37.552308561482235;
@@ -22,20 +64,69 @@ const Map=()=>{
         makeMap(latitude, longitude);
     }, []);
 
-    // 컴포넌트 렌더링 시, 특정 작업을 실행시키는 Hook
-    // useEffect(()=>{
-    //     let container = document.getElementById('map');
-    //     let options = {
-    //         center: new kakao.maps.LatLng(37.552308561482235, 126.94784341756501),
-    //         level: 3
-    //     };
-    //     let map = new kakao.maps.Map(container, options);
-    // }, []);
+    const [theme, setTheme] = useState('dark');
+    const [current, setCurrent] = useState('1');
+    const changeTheme = (value) => {
+        setTheme(value ? 'dark' : 'light');
+    };
+    const onClick = (e) => {
+        console.log('click ', e);
+        setCurrent(e.key);
+    };
 
     return (
-        <div>
-            <div id="map" style={{width:"500px", height:"400px"}}/>
-        </div>
+        <>
+            {/*상단 메뉴*/}
+            <Menu mode="horizontal" defaultSelectedKeys={['mail']}>
+                <Menu.Item key="menu" icon={<HomeOutlined/>}>
+                    Home
+                </Menu.Item>
+                <Menu.SubMenu key="SubMenu" title="About" icon={<SolutionOutlined/>}>
+                    <Menu.Item key="three" icon={<BarsOutlined/>}>
+                        Introduce
+                    </Menu.Item>
+                    <Menu.Item key="two" icon={<TeamOutlined/>}>
+                        Members
+                    </Menu.Item>
+                    <Menu.Item key="four" icon={<ToolOutlined/>}>
+                        Tech Stack
+                    </Menu.Item>
+                </Menu.SubMenu>
+                <Menu.Item key="menu2" icon={<CompassOutlined/>}>
+                    Map
+                </Menu.Item>
+                <Menu.Item key="menu3" icon={<SettingOutlined/>}>
+                    Mypage
+                </Menu.Item>
+            </Menu>
+            {/*지도*/}
+            <div id="map" style={{width: "90%", height: "90%", position: "relative", zIndex: 1}}>
+                <Switch
+                    checked={theme === 'dark'}
+                    onChange={changeTheme}
+                    checkedChildren="Dark"
+                    unCheckedChildren="Light"
+                    style={{
+                        zIndex: 2
+                    }}
+                />
+                <br/>
+                <br/>
+                <Menu
+                    theme={theme}
+                    onClick={onClick}
+                    style={{
+                        width: 256,
+                        position: "relative",
+                        zIndex: 2
+                    }}
+                    defaultOpenKeys={['sub1']}
+                    selectedKeys={[current]}
+                    mode="inline"
+                    items={items}
+                />
+            </div>
+        </>
     )
 }
 
