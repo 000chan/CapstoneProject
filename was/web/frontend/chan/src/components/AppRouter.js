0d5f242cc4a0f6f7e90694cabcc05c279b80ec7f
Chan <69744429+000chan@users.noreply.github.com>
2022-11-12 12:32:27 +0900

    react 라우터 수정

--- was/web/frontend/chan/src/components/AppRouter.js
+++ was/web/frontend/chan/src/components/AppRouter.js
@@ -42,7 +42,7 @@ const DynamicRoute = (props) => {
 		// http://localhost/user/login > path=/user/login
 		(path) =>
 			// url 페이지 컴포넌트 라우팅
-			import("../pages" + ( path.split("/")[2] != undefined ? (path) : (path+path) ))
+			import("../pages" + "/" + path.split('/')[path.split('/').length - 1] + "/" + path.split('/')[path.split('/').length - 1])
 				.then((module) => module.default)
 				.catch((e) => {
 					// "http://localhost/"인 경우 main 페이지로 redirect
