cf3b604862d28bc90db1f00e398a435f94847856
sypaik-dev <sypaik.dev@gmail.com>
2022-11-22 17:35:42 +0900

    Add home css styling

--- was/web/frontend/chan/src/pages/home/home.js
+++ was/web/frontend/chan/src/pages/home/home.js
@@ -1,14 +1,39 @@
 import React from 'react';
-import 'antd/dist/antd.css';
+// import 'antd/dist/antd.css';
 import {TopMenu} from '../../components/common'
+import './home.css'
+import image from '../../../../../static/images/home_background.jpg'
+
 
 const Main = () => {
   return (
 	<>
 		<TopMenu></TopMenu>
-		<h1>Main</h1>
+			{/* <header style={ HeaderStyle }>   */}
+			<div className="main-content"
+      style={{ backgroundImage:`url(${image})` }}>
+				<h1 className="main-title">치매 환자 추적 서비스</h1>
+			</div>
+            {/* <div className="buttons text-center">
+                <Link to="/login">
+                    <button className="primary-button" id="login_btn">로그인</button>
+                </Link>
+                <Link to="/register">
+                    <button className="primary-button" id="reg_btn"><span>회원가입</span></button>
+                </Link>
+            </div>
+         </header> */}
 	</>
   );
 }
+
+// const HeaderStyle = {
+//     width: "100%",
+//     height: "100vh",
+//     background: `url(../)`,
+//     backgroundPosition: "center",
+//     backgroundRepeat: "no-repeat",
+//     backgroundSize: "cover"
+// }
 
 export default Main;
