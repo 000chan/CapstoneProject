c9a415f1230c6c77f9f1112283d45612d5c8afdd
sypaik-dev <sypaik.dev@gmail.com>
2022-11-23 22:29:50 +0900

    주석처리 제거

--- was/web/frontend/chan/src/pages/register/register.js
+++ was/web/frontend/chan/src/pages/register/register.js
@@ -29,176 +29,122 @@ function RegisterForm() {
 
 
     return (
-        // <>
-        //     <Form
-        //         labelCol={{
-        //             span: 8,
-        //         }}
-        //         wrapperCol={{
-        //             span: 8,
-        //         }}
-        //         size="large"
-        //         layout="horizontal"
-        //         style={{
-        //             width: "100%",
-        //             margin: "3% 0 0 0",
-        //         }}
-        //         onFinish={onFinish}
-        //         onFinishFailed={onFinishFailed}
-        //         autoComplete="off"
-        //     >
-        //         <Form.Item
-        //             label="아이디"
-        //             name="registerid"
-        //             rules={[
-        //                 {required:true, message:"아이디를 입력해주세요"}
-        //             ]}
-        //         >
-        //             <Input placeholder=""/>
-        //         </Form.Item>
+        <>
+            <Form
+                labelCol={{
+                    span: 8,
+                }}
+                wrapperCol={{
+                    span: 8,
+                }}
+                size="large"
+                layout="horizontal"
+                style={{
+                    width: "100%",
+                    margin: "3% 0 0 0",
+                }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+            >
+                <Form.Item
+                    label="아이디"
+                    name="registerid"
+                    rules={[
+                        {required:true, message:"아이디를 입력해주세요"}
+                    ]}
+                >
+                    <Input placeholder=""/>
+                </Form.Item>
 
-        //         <Form.Item
-        //             label="비밀번호"
-        //             name="registerpw"
-        //             rules={[
-        //                 {required:true, message:"비밀번호를 입력해주세요"}
-        //             ]}
-        //         >
-        //             <Input placeholder=""/>
-        //         </Form.Item>
+                <Form.Item
+                    label="비밀번호"
+                    name="registerpw"
+                    rules={[
+                        {required:true, message:"비밀번호를 입력해주세요"}
+                    ]}
+                >
+                    <Input placeholder=""/>
+                </Form.Item>
 
-        //         <Form.Item
-        //             label="비밀번호 확인"
-        //             name="registerpwconfirm"
-        //             rules={[
-        //                 {required:true, message:"동일한 비밀번호를 입력해주세요"}
-        //             ]}
-        //         >
-        //             <Input placeholder="동일한 비밀번호를 입력해주세요"/>
-        //         </Form.Item>
+                <Form.Item
+                    label="비밀번호 확인"
+                    name="registerpwconfirm"
+                    rules={[
+                        {required:true, message:"동일한 비밀번호를 입력해주세요"}
+                    ]}
+                >
+                    <Input placeholder="동일한 비밀번호를 입력해주세요"/>
+                </Form.Item>
 
-        //         <Form.Item
-        //             label="이름"
-        //             name="registername"
-        //             rules={[
-        //                 {required:true, message:"이름을 입력해주세요"}
-        //             ]}
-        //         >
-        //             <Input placeholder=""/>
-        //         </Form.Item>
+                <Form.Item
+                    label="이름"
+                    name="registername"
+                    rules={[
+                        {required:true, message:"이름을 입력해주세요"}
+                    ]}
+                >
+                    <Input placeholder=""/>
+                </Form.Item>
 
-        //         <Form.Item
-        //             label="핸드폰 번호"
-        //             name="registerphonenumber"
-        //             rules={[
-        //                 {required:true, message:"핸드폰 번호를 입력해주세요"}
-        //             ]}
-        //         >
-        //             <Input placeholder=""/>
-        //         </Form.Item>
+                <Form.Item
+                    label="핸드폰 번호"
+                    name="registerphonenumber"
+                    rules={[
+                        {required:true, message:"핸드폰 번호를 입력해주세요"}
+                    ]}
+                >
+                    <Input placeholder=""/>
+                </Form.Item>
 
-        //         <Form.Item
-        //             label="생년월일"
-        //             name="registerbirthdate"
-        //             rules={[
-        //                 {required:true, message:"생년월일을 입력해주세요"}
-        //             ]}>
-        //             <DatePicker placeholder="" style={{width:"100%"}}/>
-        //         </Form.Item>
+                <Form.Item
+                    label="생년월일"
+                    name="registerbirthdate"
+                    rules={[
+                        {required:true, message:"생년월일을 입력해주세요"}
+                    ]}>
+                    <DatePicker placeholder="" style={{width:"100%"}}/>
+                </Form.Item>
 
-        //         <Form.Item
-        //             label="이메일"
-        //             name="registeremail"
-        //             rules={[
-        //                 {required:true, message:"이메일을 입력해주세요"}
-        //             ]}>
-        //             <Input placeholder=""/>
-        //         </Form.Item>
+                <Form.Item
+                    label="이메일"
+                    name="registeremail"
+                    rules={[
+                        {required:true, message:"이메일을 입력해주세요"}
+                    ]}>
+                    <Input placeholder=""/>
+                </Form.Item>
 
-        //         <Form.Item
-        //             label="null"
-        //             name="checkTerms"
-        //             style={{textAlign:"center"}}>
-        //             <Checkbox
-        //                 checked={acceptTerms}
-        //                 onChange={(e) => setAcceptTerms(e.target.checked)}
-        //             >
-        //                 개인정보 수집에 동의합니다.
-        //             </Checkbox>
-        //         </Form.Item>
+                <Form.Item
+                    label="null"
+                    name="checkTerms"
+                    style={{textAlign:"center"}}>
+                    <Checkbox
+                        checked={acceptTerms}
+                        onChange={(e) => setAcceptTerms(e.target.checked)}
+                    >
+                        개인정보 수집에 동의합니다.
+                    </Checkbox>
+                </Form.Item>
 
-        //         <Form.Item
-        //             label="null"
-        //             name="register"
-        //             style={{textAlign:"center"}}>
-        //             <Button
-        //                 onValuesChange={buttonChange}
-        //                 disabled={!acceptTerms}
-        //                 type="primary"
-        //                 htmlType="submit"
-        //             >
-        //                 가입하기
-        //             </Button>
-        //         </Form.Item>
-        //     </Form>
-        // </>
-
-
- <div className="register-content">
-<h2>회원가입</h2>
-<h5>Create your personal account</h5>
-<form action="/home"> 
-    <p>
-        <label>아이디</label><br/>
-        <input type="registerid" name ="registerid" 
-        placeholder="아이디를 입력해주세요" required />
-    </p>
-    <p>
-        <label>비밀번호</label><br/>
-        <input type="password" name="registerpw"
-       placeholder="비밀번호를 입력해주세요" required />
-    </p>
-    <p>
-        <label>비밀번호 확인</label><br/>
-        <input type="password" name="registerpwconfirm"
-        placeholder="동일한 비밀번호를 입력해주세요" required />
-    </p>
-    <p>
-        <label>보호자 이름</label><br/>
-        <input type="guardian" name="registername"  placeholder="이름을 입력해주세요" required />
-    </p>
-    <p>
-        <label>보호자 연락처</label><br/>
-        <input type="phone" name=" registerphonenumber" placeholder="핸드폰 번호를 입력해주세요" required />
-    </p>
-    <p>
-        <label>보호자 비상연락처</label><br/>
-        <input type="emergency" placeholder="생년월일을 입력해주세요" required />
-    </p>
-    <p>
-        <label>보호자 이메일</label><br/>
-        <input type="email" name="registeremail"
-        placeholder="이메일을 입력해주세요" required />
-    </p>
-    <p>
-        <label>보호자 주소</label><br/>
-        <input type="checkbox" placeholder="보호자 주소" required />
-    </p>
-   
-    <p>
-        <button id="sub_btn" type="submit"  onValuesChange={buttonChange}
-        onChange={(e) => setAcceptTerms(e.target.checked)}
-        >회원가입</button>
-    </p>
-</form>
- <footer> 
-    {/* <p><Link to="/">Back to Homepage</Link></p> */}
- </footer> 
-<footer></footer>
-</div>  
+                <Form.Item
+                    label="null"
+                    name="register"
+                    style={{textAlign:"center"}}>
+                    <Button
+                        onValuesChange={buttonChange}
+                        disabled={!acceptTerms}
+                        type="primary"
+                        htmlType="submit"
+                    >
+                        가입하기
+                    </Button>
+                </Form.Item>
+            </Form>
+        </>
 
 
-
+ 
     )
 }
 
