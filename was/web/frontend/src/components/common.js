206494501463bc81e5eefc34678df19658ed9979
Chan <69744429+000chan@users.noreply.github.com>
2022-11-27 01:41:13 +0900

    add logout button

--- was/web/frontend/src/components/common.js
+++ was/web/frontend/src/components/common.js
@@ -13,42 +13,52 @@ import {
     KeyOutlined,
     IdcardOutlined,
     RocketOutlined,
+    UnlockOutlined,
 } from "@ant-design/icons";
 
 // 공통 컴포넌트
 
 function TopMenu() {
     return (
-        <Menu mode="horizontal" defaultSelectedKeys={['topMenu']}>
-            <Menu.Item key="menu1" icon={<HomeOutlined/>} style={{width:200, textAlign:"center"}} onClick={()=>{window.location.replace("/home")}}>
-                홈
-            </Menu.Item>
-            <Menu.SubMenu key="SubMenu1" title="시작하기" icon={<RocketOutlined/>} style={{width:200, textAlign:"center"}}>
-                <Menu.Item key="sub1-1" icon={<KeyOutlined/>} onClick={()=>{window.location.replace("/user/login")}}>
-                    로그인
+        <>
+            <Menu mode="horizontal" defaultSelectedKeys={['topMenu']}>
+                <Menu.Item key="menu1" icon={<HomeOutlined/>} style={{width:200, textAlign:"center"}} onClick={()=>{window.location.replace("/home")}}>
+                    홈
                 </Menu.Item>
-                <Menu.Item key="sub1-2" icon={<IdcardOutlined/>} onClick={()=>{window.location.replace("/user/register")}}>
-                    회원가입
+                <Menu.SubMenu key="SubMenu1" title="시작하기" icon={<RocketOutlined/>} style={{width:200, textAlign:"center"}}>
+                    <Menu.Item key="sub1-1" icon={<KeyOutlined/>} onClick={()=>{window.location.replace("/user/login")}}>
+                        로그인
+                    </Menu.Item>
+                    <Menu.Item key="sub1-2" icon={<IdcardOutlined/>} onClick={()=>{window.location.replace("/user/register")}}>
+                        회원가입
+                    </Menu.Item>
+                </Menu.SubMenu>
+                <Menu.SubMenu key="SubMenu2" title="소개" icon={<SolutionOutlined/>} style={{width:200, textAlign:"center"}}>
+                    <Menu.Item key="sub2-1" icon={<BarsOutlined/>} onClick={()=>{window.location.replace("/service/about")}}>
+                        서비스
+                    </Menu.Item>
+                    <Menu.Item key="sub2-2" icon={<TeamOutlined/>} onClick={()=>{window.location.replace("/service/project")}}>
+                        멤버
+                    </Menu.Item>
+                    <Menu.Item key="sub2-3" icon={<ToolOutlined/>} onClick={()=>{window.location.replace("/service/tech")}}>
+                        기술
+                    </Menu.Item>
+                </Menu.SubMenu>
+                <Menu.Item key="menu2" icon={<CompassOutlined/>} style={{width:200, textAlign:"center"}} onClick={()=>{window.location.replace("/service/map")}}>
+                    지도 서비스
                 </Menu.Item>
-            </Menu.SubMenu>
-            <Menu.SubMenu key="SubMenu2" title="소개" icon={<SolutionOutlined/>} style={{width:200, textAlign:"center"}}>
-                <Menu.Item key="sub2-1" icon={<BarsOutlined/>} onClick={()=>{window.location.replace("/service/about")}}>
-                    서비스
+                <Menu.Item key="menu3" icon={<SettingOutlined/>} style={{width:200, textAlign:"center"}} onClick={()=>{window.location.replace("/service/mypage")}}>
+                    마이페이지
                 </Menu.Item>
-                <Menu.Item key="sub2-2" icon={<TeamOutlined/>} onClick={()=>{window.location.replace("/service/project")}}>
-                    멤버
-                </Menu.Item>
-                <Menu.Item key="sub2-3" icon={<ToolOutlined/>} onClick={()=>{window.location.replace("/service/tech")}}>
-                    기술
-                </Menu.Item>
-            </Menu.SubMenu>
-            <Menu.Item key="menu2" icon={<CompassOutlined/>} style={{width:200, textAlign:"center"}} onClick={()=>{window.location.replace("/service/map")}}>
-                지도 서비스
-            </Menu.Item>
-            <Menu.Item key="menu3" icon={<SettingOutlined/>} style={{width:200, textAlign:"center"}} onClick={()=>{window.location.replace("/service/mypage")}}>
-                마이페이지
-            </Menu.Item>
-        </Menu>
+                {
+                    localStorage.getItem("user") ?
+                        <Menu.Item key="menu4" icon={<UnlockOutlined/>} style={{width:200, textAlign:"center"}} onClick={()=>{window.location.replace("/"); localStorage.removeItem("user")}}>
+                        로그아웃
+                        </Menu.Item>
+                        : null
+                }
+            </Menu>
+        </>
     );
 }
 
