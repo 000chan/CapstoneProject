be203a3b4e55ab1bfafb4817ac1c12b909eccb9b
Chan <69744429+000chan@users.noreply.github.com>
2022-11-27 19:59:04 +0900

    modify admin file (service app)

--- was/web/backend/backend_appService/admin.py
+++ was/web/backend/backend_appService/admin.py
@@ -3,47 +3,8 @@ from .models import MissingAnnouncement, MissingInfo, MissingProtector, Pastpath
 
 # django admin page에 등록될 model들
 
-@admin.register(MissingAnnouncement)
-class MissingAnnouncementAdmin(admin.ModelAdmin):
-    list_display = (
-        'announcementnum',
-        'missingname',
-        'missingimage',
-        'enroll_time',
-        'protectornum',
-        'infonum'
-    )
-
-@admin.register(MissingInfo)
-class MissingInfoAdmin(admin.ModelAdmin):
-    list_display = (
-        'infonum',
-        'disappearancetime',
-        'disappearancearea'
-    )
-
-@admin.register(MissingProtector)
-class MissingProtectorAdmin(admin.ModelAdmin):
-    list_display = (
-        'protectornum',
-        'protectorname',
-        'protectorphonenum'
-    )
-
-@admin.register(Pastpath)
-class Pastpath(admin.ModelAdmin):
-    list_display = (
-        'targetnum',
-        'usernum',
-        'devicename',
-        'signaledtime',
-        'latitude',
-        'longitude'
-    )
-
-@admin.register(Search)
-class Search(admin.ModelAdmin):
-    list_display = (
-        'targetnum',
-        'usernum'
-    )
+admin.site.register(MissingAnnouncement)
+admin.site.register(MissingInfo)
+admin.site.register(MissingProtector)
+admin.site.register(Pastpath)
+admin.site.register(Search )
