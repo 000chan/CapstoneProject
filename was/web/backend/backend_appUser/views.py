b54c532d26ce7719b1bc899127f5aee7d82606df
Chan <69744429+000chan@users.noreply.github.com>
2022-11-27 20:10:13 +0900

    refactoring

--- was/web/backend/backend_appUser/views.py
+++ was/web/backend/backend_appUser/views.py
@@ -2,34 +2,54 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializer import UserSerializer
-from .models import User
+from .models import User, Target
 import datetime
 from argon2 import PasswordHasher
+import json
+from django.core import serializers
 
 class Login(APIView):
     def post(self, request):
+        # request data
         id = request.data["id"]
         password = request.data["pass_field"]
 
-        if User.objects.get(id=id):
+        # request data와 일치하는 회원정보가 있는지 검사
+        try:
             user = User.objects.get(id=id)
-        else:
+            ("===SUCCESS: 일치하는 아이디를 찾았습니다!===")
+        except:
             print("===ERROR: 아이디 데이터가 올바르지 않습니다.===")
             return Response(
                 { "message" : "아이디가 존재하지 않습니다." },
                 status=status.HTTP_400_BAD_REQUEST
             )
+        # if User.objects.get(id=id):
+        #     user = User.objects.get(id=id)
+        #     ("===SUCCESS: 일치하는 아이디를 찾았습니다!===")
+        # else:
+        #     print("===ERROR: 아이디 데이터가 올바르지 않습니다.===")
+        #     return Response(
+        #         { "message" : "아이디가 존재하지 않습니다." },
+        #         status=status.HTTP_400_BAD_REQUEST
+        #     )
 
-        if PasswordHasher().verify(user.pass_field, password=password):
-            print("===login success===")
+        # 회원이 맞는 경우, 비밀번호 검사
+        try:
+            PasswordHasher().verify(user.pass_field, password=password)
+            print("===SUCCESS: 로그인 성공!===")
             return Response(user.id, status=status.HTTP_200_OK)
-        else:
+        except:
             print("===ERROR: 비밀번호 데이터가 올바르지 않습니다.===")
-
-        return Response(
+            return Response(
             { "message" : "입력된 비밀번호가 올바르지 않습니다. 다시 입력해주세요" },
             status=status.HTTP_400_BAD_REQUEST
-        )
+            )
+        # if PasswordHasher().verify(user.pass_field, password=password):
+        #     print("===SUCCESS: 로그인 성공!===")
+        #     return Response(user.id, status=status.HTTP_200_OK)
+        # else:
+        #     print("===ERROR: 비밀번호 데이터가 올바르지 않습니다.===")
 
 class Register(APIView):
     def post(self, request):
@@ -41,7 +61,7 @@ class Register(APIView):
                 status=status.HTTP_400_BAD_REQUEST
             )
         # e_mail 유효성 체크
-        elif request.data["e_mail"].find("@") == -1:
+        elif request.data["e_mail"].find("@") == -1 or request.data["e_mail"].find(".com") == -1:
             print("===ERROR: 'e_mail' 데이터가 올바르지 않습니다.===")
             return Response(
                 { "message" : "이메일 주소가 올바르지 않습니다. 다시 입력해주세요." },
@@ -58,6 +78,7 @@ class Register(APIView):
                 )
             else:
                 request.data["userage"] = str(userage)
+                print('===SUCCEESS: userage 계산에 성공했습니다!===')
 
         # pass_field 데이터 암호화
         request.data["pass_field"] = PasswordHasher().hash(request.data["pass_field"])
@@ -68,7 +89,7 @@ class Register(APIView):
         # request.data 유효성 검사
         if serializer.is_valid():
             user = serializer.save()
-            print('===register success===')
+            print('===SUCCEESS: 회원가입에 성공했습니다!===')
             return Response(status=status.HTTP_200_OK)
 
         # request.data 유효성 검사 예외처리
@@ -85,5 +106,5 @@ class Register(APIView):
                     message.append("이미 존재하는 이메일입니다.")
                     print("===ERROR: 이미 존재하는 이메일 데이터입니다.===")
 
-        print("===register fail===")
+        print("===ERROR: 회원가입에 실패했습니다.===")
         return Response(message, status=status.HTTP_400_BAD_REQUEST)
