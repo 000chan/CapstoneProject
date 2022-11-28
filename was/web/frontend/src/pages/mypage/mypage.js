e3c59e5be6ce087fcc9e88e8c49867ae63839904
Chan <69744429+000chan@users.noreply.github.com>
2022-11-29 00:40:32 +0900

    react

--- was/web/frontend/src/pages/mypage/mypage.js
+++ was/web/frontend/src/pages/mypage/mypage.js
@@ -1,4 +1,4 @@
-import { Avatar, Button, Card, Tabs } from 'antd';
+import { Avatar, Button, Card, Tabs, Form, Input, DatePicker, Checkbox } from 'antd';
 import React, { useEffect, useState } from 'react';
 import { TopMenu } from '../../components/common';
 import axios from "axios";
@@ -12,9 +12,82 @@ if(localStorage.getItem("user")){
     params=localStorage.getItem("user");
 }
 
+// function ModifyUserInfo({mypageData}) {
+//     let userData = mypageData["user"][0]
+//     let targetData = mypageData["target"][0]
+//     // registerForm input value 검사
+//     const onFinish = (values) => {
+//         axios
+//             .put("http://127.0.0.1:8000/user/mypage/", values)
+//             .then(function (response) {
+//                 console.log(response);
+//             })
+//             .catch(function (response) {
+//                 console.log(response);
+//             })
+//     };
+//     const onFinishFailed = (errorInfo) => {
+//         console.log('Failed:', errorInfo);
+//     };
+//     return (
+//         <>
+//             <Form
+//                 labelCol={{
+//                     span: 8,
+//                 }}
+//                 wrapperCol={{
+//                     span: 8,
+//                 }}
+//                 size="large"
+//                 layout="horizontal"
+//                 style={{
+//                     width: "100%",
+//                     margin: "3% 0 0 0",
+//                 }}
+//                 autoComplete="off"
+//                 onFinish={onFinish}
+//                 onFinishFailed={onFinishFailed}
+//             >
+
+//                 <Form.Item
+//                     label="이름"
+//                     name="username"
+//                     initialValue={userData.username}
+//                     rules={[
+//                         { required: true, message: "이름을 입력해주세요" }
+//                     ]}
+//                 >
+//                     <Input placeholder="이름을 입력해주세요" />
+//                 </Form.Item>
+
+//                 <Form.Item
+//                     label="핸드폰 번호"
+//                     name="userphonenum"
+//                     initialValue={userData.userphonenum}
+//                     rules={[
+//                         { required: true, message: "핸드폰 번호를 입력해주세요" }
+//                     ]}
+//                 >
+//                     <Input placeholder="핸드폰 번호를 입력해주세요" />
+//                 </Form.Item>
+
+//                 <Form.Item
+//                     label="이메일"
+//                     name="e_mail"
+//                     initialValue={userData.e_mail}
+//                     rules={[
+//                         { required: true, message: "이메일을 입력해주세요" }
+//                     ]}>
+//                     <Input placeholder="이메일을 입력해주세요" />
+//                 </Form.Item>
+//             </Form>
+//         </>
+//     );
+// }
+
 const Mypage = () => {
     const [mypageData, setMypageData] = useState([]);   // 회원정보를 위한 상태
-    const [modify, setModify] = useState(0);            // 회원정보 수정을 위한 상태
+    // const [modify, setModify] = useState(0);            // 회원정보 수정을 위한 상태
 
     // axios 통신
     useEffect(() => {
@@ -29,7 +102,7 @@ const Mypage = () => {
     }, []);
 
 
-    // axios response data 가져오는 부분
+    // axios response data 가져오기
     let username;
     let usercontent = [];
     let targetcontent = [];
@@ -52,18 +125,20 @@ const Mypage = () => {
         targetcontent = tmpStrT.join('');
     }
 
-    // 회원정보 탭 생성
+    // 탭 라벨 생성
     let labels = [];
     let label1 = "내 정보";
     let label2 = "보호대상 정보";
     labels.push(label1);
     labels.push(label2);
     
+    // 탭 내용 생성
     let contents = []
     let userDataContent = usercontent;
     let targetDataContent = targetcontent;
     contents.push(userDataContent);
     contents.push(targetDataContent);
+
     const items = new Array(contents.length).fill(null).map((_, i) => {
         let id = String(i + 1);
         return {
@@ -78,7 +153,7 @@ const Mypage = () => {
             ),
         };
     });
-    
+
     return (
         <>
             <TopMenu/>
@@ -95,34 +170,37 @@ const Mypage = () => {
                             src={man}
                         />
                     }
-                    actions={[
-                        modify==0 ?
-                        <Button
-                            modify={modify}
-                            onClick={() => {
-                                if(modify==0){
-                                    setModify(modify+1);
-                                } else {
-                                    setModify(modify-1);
-                                }
-                            }}
-                        >
-                            개인정보 변경하기
-                        </Button>
-                        :
-                        <Button
-                            modify={modify}
-                            onClick={() => {
-                                if(modify==0){
-                                    setModify(modify+1);
-                                } else {
-                                    setModify(modify-1);
-                                }
-                            }}
-                        >
-                            개인정보 저장하기
-                        </Button>
-                    ]}
+                    // modify={modify}
+                    // actions={[
+                    //     modify==0 ?
+                    //     <Button
+                    //         mypageData={mypageData}
+                    //         modify={modify}
+                    //         onClick={() => {
+                    //             setModify(1)    // modify=0 > modify=1
+                    //         }}
+                    //     >
+                    //         개인정보 변경하기
+                    //     </Button>
+                    //     :
+                    //     <Button
+                    //         mypageData={mypageData}
+                    //         modify={modify}
+                    //         onClick={() => {
+                    //             setModify(0)    // modify=0 > modify=1
+                    //             axios
+                    //                 .put("http://127.0.0.1:8000/user/mypage/", values)
+                    //                 .then(function (response) {
+                    //                 })
+                    //                 .catch(function (response) {
+                    //                 })
+                    //         }}
+                    //         type="primary"
+                    //         htmlType="submit"
+                    //     >
+                    //         개인정보 저장하기
+                    //     </Button>
+                    // ]}
                 >
                     <Meta
                         avatar={<Avatar src={man}/>}
@@ -130,9 +208,30 @@ const Mypage = () => {
                     />
                 </Card>
                 <Tabs type="card"
-                    items={items}
-                    style={{width: "900px", float: "right", margin: "3% 5% 0 0", whiteSpace:"pre-line"}}
+                        items={items}
+                        style={{width: "900px", float: "right", margin: "3% 5% 0 0", whiteSpace:"pre-line"}}
                 />
+                {/* {
+                    modify==0?
+                    <Tabs type="card"
+                        items={items}
+                        style={{width: "900px", float: "right", margin: "3% 5% 0 0", whiteSpace:"pre-line"}}
+                    />
+                :
+                    <>
+                        <Tabs type="card"
+                            items={[
+                                {"label":"내 정보", "key":"1"},
+                                {"label":"보호대상 정보", "key":"2"}
+                            ]}
+                            style={{width: "900px", float: "right", margin: "3% 5% 0 0", whiteSpace:"pre-line"}}
+                        />
+                        <br/><br/><br/><br/>
+                        <ModifyUserInfo
+                            mypageData={mypageData}
+                        ></ModifyUserInfo>
+                    </>
+                } */}
             </div>
         </>
     );
