d7666bba5e5cd867bfafdf80378a97d5eff437ee
LyMast <lymast3578@naver.com>
2022-12-08 05:47:12 +0900

    Update

--- server/test.cpp
+++ server/test.cpp
@@ -1,68 +1,47 @@
 #include <stdio.h>
-#include <mysql.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <fcntl.h>
 
-int main(int argc, char** argv)
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <errno.h>
+
+
+char* SERVERIP = (char*)"127.0.0.1";
+#define SERVERPORT	12000
+#define BUFSIZE		512
+
+int main()
 {
-  MYSQL_RES *result;
-  MYSQL_ROW row;
-  MYSQL *connection, mysql;
-  int state;
+	int retval;
 
-  /* Connect Locol host Database */
-  mysql_init(&mysql);
-  connection = mysql_real_connect(&mysql, "localhost", "root",
-			"DKUcap23!","AKBScapstone", 0, 0, 0);
-  if(connection == NULL)
-  {
-    printf(mysql_error(&mysql));
-    return 1;
-  }
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	
+	struct sockaddr_in serveraddr;
+	memset(&serveraddr, 0, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	inet_pton(AF_INET, SERVERIP, &serveraddr.sin_addr);
+	serveraddr.sin_port = htons(SERVERPORT);
 
-  state = mysql_query(connection, "SELECT * FROM Target");
-  if(state != 0)
-  {
-    printf(mysql_error(connection));
-    return 1;
-  }
-
-  result = mysql_store_result(connection);
-
-  printf("ROWS : %ld\n", mysql_num_rows(result));
-  while( (row = mysql_fetch_row(result)) != NULL)
-  {
-    /*
-    printf("id : %s, val = %s\n", (row[0] ? row[0] : "NULL")
-			, (row[1] ? row[1] : "NULL"));
-    */
-    printf("%s\n", row[2]);
-  }
-
-  state = mysql_query(connection, "SELECT * FROM User");
-  if(state != 0)
-  {
-    printf(mysql_error(connection));
-    return 1;
-  }
-
-  result = mysql_store_result(connection);
-
-  printf("ROWS : %ld\n", mysql_num_rows(result));
-  while( (row = mysql_fetch_row(result)) != NULL)
-  {
-    /*
-    printf("id : %s, val = %s\n", (row[0] ? row[0] : "NULL")
-                        , (row[1] ? row[1] : "NULL"));
-    */
-    printf("%s\n", row[3]);
-  }
+	connect(sock, (struct sockaddr*)&serveraddr,sizeof(serveraddr));
 
 
-  mysql_free_result(result);
-  mysql_close(connection);
-  printf("Done.\n");
+	char buf[BUFSIZE + 1] = "0 Device1 30.144 30.155 \0";
+	while (1)
+	{
+
+		send(sock, buf, (int)strlen(buf), 0);
+	        printf("%s\n",buf);
 
 
 
+		sleep(3);
+	}
 
-  return 0;
+
 }
